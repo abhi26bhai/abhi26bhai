bb1c859b21f562e9046d066d2c6b8423aca0d6ba
abhi26bhai <abhi26bhai@gmail.com>
2024-03-27 08:39:57 +0545

    for loop program

---
+++ CLASS_9_PROGRAMS/c_programs/atozprint.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    for (char ch='A'; ch<='Z'; ch++)
+    {
+printf("%c\n", ch);
+    }
+    
+    return 0;
+}
---
+++ CLASS_9_PROGRAMS/c_programs/print5timeiloveyou.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    for (int i =1; i<=50; i = i+1)
+    {
+    printf("I love you\n");
+    }
+    
+    return 0;
+}
